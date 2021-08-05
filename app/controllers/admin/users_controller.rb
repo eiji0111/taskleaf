9dbf40042db2fae826085410b696dc03bf303139
eiji0111 <ee111fender@gmail.com>
2021-08-05 23:41:17 +0900

    [Update]user_controller and admin_index

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,7 +1,7 @@
 class Admin::UsersController < ApplicationController
-  
-  def index
 
+  def index
+    @users = User.all
   end
   
   def new
@@ -18,9 +18,27 @@ class Admin::UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def show
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      redirect_to admin_user_url(@user), notice: "ユーザー「#{user.name}」を更新しました。"
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to admin_users_url, notice: "ユーザー「#{user.name}」を削除しました。"
   end
 
   private
