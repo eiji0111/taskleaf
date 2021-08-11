997895d096527c3a4596191ef9fcac6d10d7bec0
eiji0111 <ee111fender@gmail.com>
2021-08-11 11:28:44 +0900

    [Finish]Lesson4

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,11 +2,10 @@ class Task < ApplicationRecord
 
   belongs_to :user
 
-  before_validation :set_nameless_name
   validates :name, presence: true, length: { maximum: 30 }
   validate :validate_name_not_including_comma
 
-  scope :recent, -> { order(created_at: DESC) }
+  scope :recent, -> { order(created_at: :desc) }
 
   private
 
