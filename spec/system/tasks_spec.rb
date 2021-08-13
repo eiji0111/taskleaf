ad03f8cccc2c7979a36c14ee32c00f2033365659
eiji0111 <ee111fender@gmail.com>
2021-08-14 00:25:46 +0900

    [Add]rspec factorybot capybara

---
+++ spec/system/tasks_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+describe 'タスク管理機能', type: :system do
+  describe '一覧機能表示' do
+    let(:user_a) { FactoryBot.create(:user, name: 'ユーザーA', email: 'a@example.com') }
+    let(:user_b) { FactoryBot.create(:user, name: 'ユーザーB', email: 'b@example.com') }
+
+    before do
+      FactoryBot.create(:task, name: '最初のタスク', user: user_a)
+
+      visit login_path
+      fill_in 'メールアドレス', with: login_user.email
+      fill_in 'パスワード', with: login_user.password
+      click_button 'ログインする'
+    end
+
+    context 'ユーザーAがログインしている時' do
+      let(:login_user) { user_a }
+
+      it 'ユーザーAが作成したタスクが表示される' do
+        expect(page).to have_content '最初のタスク'
+      end
+    end
+
+    context 'ユーザーBがログインしている時' do
+      let(:login_user) { user_b }
+
+      it 'ユーザーAが作成したタスクが表示されない' do
+        expect(page).not_to have_content '最初のタスク'
+      end
+    end
+  end
+end
